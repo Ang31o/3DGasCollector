c6a21bb6828b53bcdbfb27ed8ae2965cdb161762
Ang31o <andjelogogic@gmail.com>
2024-04-10 18:53:44 +0200

    Minor change

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ new Engine({
   canvas: document.querySelector('#canvas') as HTMLCanvasElement,
   experience: Game,
   info: {
-    github: 'https://github.com/Ang31o/3DRacer',
-    description: 'A simple car racing game, made with Three.js + Cannon.js',
+    github: 'https://github.com/Ang31o/3DGasCollector',
+    description: 'A simple car racing game, made with Three.js and Cannon.js',
     documentTitle: 'Gas Collector',
     title: 'Gas Collector',
   },
