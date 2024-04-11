734be76e58a5354d4a79cfbd8d5bab6f15b09deb
Ang31o <andjelogogic@gmail.com>
2024-04-11 21:21:08 +0200

    Removing effect composer

--- src/engine/RenderEngine.ts
+++ src/engine/RenderEngine.ts
@@ -1,13 +1,10 @@
 import { WebGLRenderer } from 'three';
 import { Engine } from './Engine';
 import * as THREE from 'three';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { GameEntity } from './GameEntity';
 
 export class RenderEngine implements GameEntity {
   private readonly renderer: WebGLRenderer;
-  composer: EffectComposer;
 
   constructor(private engine: Engine) {
     this.renderer = new WebGLRenderer({
@@ -16,30 +13,19 @@ export class RenderEngine implements GameEntity {
     });
 
     this.renderer.outputColorSpace = THREE.SRGBColorSpace;
-    // this.renderer.toneMapping = THREE.CineonToneMapping;
-    // this.renderer.toneMappingExposure = 1.75;
     this.renderer.shadowMap.enabled = true;
     this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     this.renderer.setClearColor('#000000');
     this.renderer.setSize(this.engine.sizes.width, this.engine.sizes.height);
     this.renderer.setPixelRatio(Math.min(this.engine.sizes.pixelRatio, 2));
-
-    this.composer = new EffectComposer(this.renderer);
-
-    const renderPass = new RenderPass(
-      this.engine.scene,
-      this.engine.camera.instance
-    );
-    this.composer.addPass(renderPass);
   }
 
   update() {
-    this.composer.render();
+    this.renderer.render(this.engine.scene, this.engine.camera.instance);
   }
 
   resize() {
     this.renderer.setSize(this.engine.sizes.width, this.engine.sizes.height);
-    this.composer.setSize(this.engine.sizes.width, this.engine.sizes.height);
-    this.composer.render();
+    this.renderer.render(this.engine.scene, this.engine.camera.instance);
   }
 }
